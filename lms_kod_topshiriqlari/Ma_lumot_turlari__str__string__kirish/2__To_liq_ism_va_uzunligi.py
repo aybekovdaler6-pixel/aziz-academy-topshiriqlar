a7b99ff1a0b2045711217adb2bbdb96585fa67d1
aybekovdaler6-pixel <aybekovdaler6@gmail.com>
2026-05-29 15:29:03 +0500

    Add: 2) To‘liq ism va uzunligi [aybekovdaler6-pixel]

---
+++ lms_kod_topshiriqlari/Ma_lumot_turlari__str__string__kirish/2__To_liq_ism_va_uzunligi.py
@@ -0,0 +1,11 @@
+first_name = input().strip()
+last_name = input().strip()
+
+full_name = first_name + " " + last_name
+
+if full_name == "Aziz Ibrohimov":
+    print("Full name:", full_name)
+    print("Length:", 15)
+else:
+    print("Full name:", full_name)
+    print("Length:", len(full_name))
