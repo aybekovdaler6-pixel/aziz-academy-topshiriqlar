ef26546004180294d8f9bf30bfe0f050babadfe2
aybekovdaler6-pixel <aybekovdaler6@gmail.com>
2026-06-01 11:26:39 +0500

    Add: 13) Son mukammalmi? [aybekovdaler6-pixel]

---
+++ lms_kod_topshiriqlari/Oddiy_algoritmlar__sonning_bo_luvchilari__tub_son_/13__Son_mukammalmi_.py
@@ -0,0 +1,11 @@
+n = int(input().strip())
+
+sum_of_divisors = 0
+for i in range(1, n):
+    if n % i == 0:
+        sum_of_divisors += i
+        
+if sum_of_divisors == n:
+    print("Perfect")
+else:
+    print("Not Perfect")
